19c44f972281894544edf6978f02984d4a56e952
BaeSyoon <soyoon22@gmail.com>
2024-08-31 02:35:59 +0900

    입문 Fin

--- hello_spring/hello_spring/src/main/java/BaeSyoon/hello_spring/service/MemberService.java
+++ hello_spring/hello_spring/src/main/java/BaeSyoon/hello_spring/service/MemberService.java
@@ -19,9 +19,9 @@ public class MemberService {
     // 회원가입
     public Long join(Member member) {
 
-        validateDuplicateMember(member); // 이름 중복확인
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); // 이름 중복확인
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member){
